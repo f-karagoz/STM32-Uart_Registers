04f6d86aa1329f594a547b5f4cc90a0b03f3a114
Furkan <fkaragoz14@gmail.com>
2022-04-19 17:17:19 +0300

    Interrupt for button.

--- F746_Uart_Registers/Core/Inc/main.h
+++ F746_Uart_Registers/Core/Inc/main.h
@@ -9,39 +9,26 @@ extern "C" {
 #include "stm32f746xx.h"
 #include "RccConfig.h"
 #include "Delay.h"
+#include "UartConfig.h"
 
-/**
- * @brief Configures UART.
- *
- */
-void UartConfig (void);
-
-/**
- * @brief Sends one char through USART3;
- *
- * @param c Actual char.
- */
-void UART3_SendChar (uint8_t c);
-
-/**
- * @brief Sends char array.
- *
- * @param data Pointer to char array.
- */
-void UART3_SendData (uint8_t * data);
-
-/**
- * @brief Gets the char from UART.
- *
- * @return Read char data.
- */
-uint8_t UART3_GetChar (void);
 
 /**
  * @brief Configures the GPIO.
  *
  */
 void GPIO_Config (void);
+
+/**
+ * @brief Configures the interrupt.
+ *
+ */
+void Interrupt_Config (void);
+
+/**
+ * @brief Interrupt handler for EXTI 15:10
+ *
+ */
+void EXTI15_10_IRQHandler(void);
 
 
 #ifdef __cplusplus
--- F746_Uart_Registers/Core/Src/main.c
+++ F746_Uart_Registers/Core/Src/main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+uint8_t flag = 0;
+
 /**
   * @brief  The application entry point.
   * @retval int
@@ -10,91 +12,23 @@ int main(void)
 	UartConfig();
 	TIM6Config();
 	GPIO_Config();
+	Interrupt_Config();
 	//uint8_t myChar = 'C';
-	uint8_t myArr[10] = "Hello\n\0";
+	uint8_t myArr[10] = "\nHello\n> \0";
 	while (1)
 	{
-		GPIOB->BSRR |= (1<<0);		// Set
-		delay_ms(500);
-		GPIOB->BSRR |= (1<<16);		// Reset
-		delay_ms(500);
-		//UART3_SendChar(myChar);
-		UART3_SendData(myArr);
-		uint8_t rxData = UART3_GetChar();
-
+		if (flag) {
+			GPIOB->BSRR |= (1<<0);		// Set
+			delay_ms(1000);
+			GPIOB->BSRR |= (1<<16);		// Reset
+			UART3_SendData(myArr);
+			uint8_t rxData = UART3_GetChar();
+			flag = 0;
+		}
+		else {
+			//GPIOB->BSRR |= (1<<16);		// Reset
+		}
 	}
-}
-
-void UartConfig (void) {
-	/******* STEPS FOLLOWED ********
-	1. Enable the UART CLOCK and GPIO CLOCK
-	2. Configure the UART PINs for Alternate Functions
-	3. Program the M bit in USART_CR1 to define the word length.
-	4. Select the desired baud rate using the USART_BRR register.
-	5. Program the number of stop bits in USART_CR2.
-	6. Enable the USART by writing the UE bit in USART_CR1 register to 1.
-	7. Set the TE bit in USART_CR1 to send an idle frame as first transmission.
-	********************************/
-	// 1. Enable the UART CLOCK and GPIO CLOCK
-	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;	// Enable USART3's clock
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;	// Enable GPIOD's clock (D port used by USART3's pins PD8,9)
-
-	// 2. Configure the UART PINs for Alternate Functions
-	GPIOD->MODER |= GPIO_MODER_MODER8_1 | GPIO_MODER_MODER9_1; //PD8 and PD9 pins set as Alternate Function
-	GPIOD->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR8 | GPIO_OSPEEDR_OSPEEDR9; //Pins setup as high speed.
-	GPIOD->AFR[1] |= (7<<0) | (7<<4); // AF7 set for pin 8 and 9. Those pins' AFR is in the AFRH register therefore AFR[1].
-
-	// 3. Program the M bit in USART_CR1 to define the word length.
-	//USART3->CR1 &= ~(USART_CR1_M0 | USART_CR1_M1);
-	USART3->CR1 &= ~USART_CR1_M; // We set the M0 and M1 as 0 for 8 bit message format.
-
-	// 4. Select the desired baud rate using the USART_BRR register.
-	USART3->CR1 &= ~USART_CR1_OVER8;	// Set the oversampling by 16 to be sure.
-	USART3->BRR |= 0x139;	// For 115200 baud rate the BRR calculation via the RM0385 s.31.5.4 formula. 0x139 = 313d
-
-	// 5. Program the number of stop bits in USART_CR2.
-	USART3->CR2 &= ~USART_CR2_STOP; // For 1 stop bit (13:12) set as 0:0
-
-	// 6. Enable the USART by writing the UE bit in USART_CR1 register to 1.
-	USART3->CR1 |= USART_CR1_UE;	// Enable USART
-
-	// 7. Set the TE bit in USART_CR1 to send an idle frame as first transmission.
-	USART3->CR1 |= USART_CR1_TE | USART_CR1_RE;	// Enable Transmit and Receive
-
-}
-
-void UART3_SendChar (uint8_t c) {
-	/*********** STEPS FOLLOWED *************
-	1. Write the data to send in the USART_TDR register (this clears the TXE bit). Repeat this
-		 for each data to be transmitted in case of single buffer.
-	2. After writing the last data into the USART_TDR register, wait until TC=1. This indicates
-		 that the transmission of the last frame is complete. This is required for instance when
-		 the USART is disabled or enters the Halt mode to avoid corrupting the last transmission.
-	****************************************/
-	if (USART3->ISR & USART_ISR_TXE) // First check if the Transmit Data Register is empty
-	{
-		USART3->TDR = c;	// Load the char to be transmitted into TDR
-		while(!(USART3->ISR & USART_ISR_TC));	// Wait for Transmit Complete flag to be set in the ISR
-	}
-}
-
-void UART3_SendData (uint8_t * data) {	// The input of the function is the address of the char array
-	while (*data != '\0') {				// We will be looping until we find NULL char
-		UART3_SendChar(*data);			// We send the value pointed by the current address
-		(data)++;						// We increment the address by one for the next 8 bit
-	}
-}
-
-uint8_t UART3_GetChar (void) {
-	/*********** STEPS FOLLOWED *************
-	1. Wait for the RXNE bit to set. It indicates that the data has been received and can be read.
-	2. Read the data from USART_DR  Register. This also clears the RXNE bit
-	****************************************/
-
-	uint8_t data;
-	while (!(USART3->ISR & USART_ISR_RXNE)); // Wait for the RX Not Empty Flag to set. Which indicates that data can be read.
-	data = USART3->RDR;
-	return data;
 }
 
 void GPIO_Config (void) {
@@ -114,5 +48,56 @@ void GPIO_Config (void) {
 	GPIOB->OTYPER |= (0<<0);	// Push-pull type for pin 0.
 	GPIOB->OSPEEDR |= (2<<0);	// High speed setup for pin 0
 	GPIOB->PUPDR &= ~((1<<1) | (1<<0)); // 1:0 set as (00) for pin 0 to be no pull up or down
+	/************ STEPS FOLLOWED ***********
+	1. Enable the GPIO CLOCK
+	2. Set the Pin as INPUT
+	3. Configure the PULL UP or DOWN
+	Note: Button connected to PC13 and hardware pull-down
+	***************************************/
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;	// Enable GPIO C Clock
+	GPIOC->MODER &= ~GPIO_MODER_MODER13;	// Set C13 as Input with bits (27:26) as 0:0
+	GPIOC->PUPDR &= ~GPIO_PUPDR_PUPDR13;	// No pull up or pull down
+	GPIOC->PUPDR |= (2 << GPIO_PUPDR_PUPDR13_Pos); // Pull down
+}
+
+void Interrupt_Config (void) {
+	/*************>>>>>>> STEPS FOLLOWED <<<<<<<<************
+	1. Enable the SYSCNFG bit in RCC register
+	2. Configure the EXTI configuration Register in the SYSCNFG
+	3. Disable the EXTI using Interrupt Mask Register (IMR)
+	4. Configure the Rising Edge / Falling Edge Trigger
+	5. Set the Interrupt Priority
+	6. Enable the interrupt
+	********************************************************/
+	// 1. Enable the SYSCNFG bit in RCC register
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	// 2. Configure the EXTI configuration Register in the SYSCNFG
+	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13_PC;
+
+	// 3. Disable the EXTI using Interrupt Mask Register (IMR)
+	EXTI->IMR |= EXTI_IMR_IM13;
+
+	// 4. Configure the Rising Edge / Falling Edge Trigger
+	EXTI->RTSR |= EXTI_RTSR_TR13; // Rising edge trigger for line 13 is set
+	EXTI->FTSR &= ~EXTI_FTSR_TR13;
+
+	// 5. Set the Interrupt Priority
+	NVIC_SetPriority(EXTI15_10_IRQn, 0);
+
+	// 6. Enable the interrupt
+	NVIC_EnableIRQ(EXTI15_10_IRQn);
+}
+
+void EXTI15_10_IRQHandler(void) {
+	/*************>>>>>>> STEPS FOLLOWED <<<<<<<<************
+	1. Check the Pin, which triggered the Interrupt
+	2. Clear the Interrupt Pending Bit
+	********************************************************/
+
+	if(EXTI->PR & EXTI_PR_PR13) {	// We are checking if this interrupt handler triggered by 13th interrupt line
+		flag = 1;
+		EXTI->PR |= EXTI_PR_PR13;	// Bit is cleared by writing 1 to it as described in the RM.
+	}
 }
 
